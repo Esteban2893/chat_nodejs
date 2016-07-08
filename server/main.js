d1edad6ffea4b91161a8f30903abf8be9b710721
Esteban2893 <estebanmora_93@hotmail.com>
2016-07-08 16:32:19 -0600

    view chat

--- server/main.js
+++ server/main.js
@@ -3,9 +3,18 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 
-server.listen(3000,function(){
+io.on('connection', function(socket){
+	console.log('Conectado con sockets');
+	socket.emit('messages', {
+		id:1,
+		text: "Hola Esteban",
+		author: "Esteban Mora"
+	});
+});
+
+server.listen(3000, function(){
 	console.log("Servidor corriendo: http://localhost:3000");
 });
