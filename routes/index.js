8f2139b35c27ec0c84ee3fc7bc15c56ccee73194
Pablo Arce Cascante <josepa_arce@hotmail.com>
2016-07-10 22:12:06 -0600

    Cute Cat

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express Chat',
+    welcome: 'Welcome to Express Chat build on Node.js + Express Framework',
+  });
 });
 
 module.exports = router;
